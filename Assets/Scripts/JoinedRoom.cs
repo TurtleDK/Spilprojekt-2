f5e740b8f40ba727b2f208efea60cd7d37b2ad53
Daniel From Kjærgaard <KongzCringe@gmail.com>
2022-03-29 15:17:24 +0200

    lol

--- Assets/Scripts/JoinedRoom.cs
+++ Assets/Scripts/JoinedRoom.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class JoinedRoom : MonoBehaviour
 {
-    [SerializeField] private GameObject spawnPoint;
+    [SerializeField] private GameObject spawnPoint1;
+    [SerializeField] private GameObject spawnPoint2;
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private GameObject Hus1;
     [SerializeField] private GameObject Hus2;
@@ -16,21 +17,20 @@ public class JoinedRoom : MonoBehaviour
     {
         if (!playerSpawned)
         {
-            for (int i = 0; i < Hus1.transform.childCount; i++)
+            for (int i = 0; i < Hus2.transform.childCount; i++)
             {
-                if (Hus1.transform.GetChild(i).name == "FirstPersonPlayer(Clone)")
+                if (Hus1.transform.GetChild(i).name.Contains(playerPrefab.name))
                 {
-                    player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.transform.position, spawnPoint.transform.rotation);
-                    player.transform.parent = Hus2.transform;
+                    player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint1.transform.position, spawnPoint1.transform.rotation);
+                    player.transform.name = "FirstPersonPlayerHus1";
                     player.transform.GetChild(1).gameObject.SetActive(true);
                     playerSpawned = true;
                 }
             }
-
             if (!playerSpawned)
             {
-                player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.transform.position, spawnPoint.transform.rotation);
-                player.transform.parent = Hus1.transform;
+                player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint2.transform.position, spawnPoint2.transform.rotation);
+                player.transform.name = "FirstPersonPlayerHus2";
                 player.transform.GetChild(1).gameObject.SetActive(true);
                 playerSpawned = true;
             }
@@ -40,18 +40,18 @@ public class JoinedRoom : MonoBehaviour
         {
             for (int i = 0; i < Hus2.transform.childCount; i++)
             {
-                if (Hus2.transform.GetChild(i).name == "FirstPersonPlayer(Clone)")
+                if (Hus2.transform.GetChild(i).name.Contains(playerPrefab.name))
                 {
-                    player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.transform.position, spawnPoint.transform.rotation);
-                    player.transform.parent = Hus1.transform;
+                    player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint1.transform.position, spawnPoint1.transform.rotation);
+                    player.transform.name = "FirstPersonPlayerHus1";
                     player.transform.GetChild(1).gameObject.SetActive(true);
                     playerSpawned = true;
                 }
             }
             if (!playerSpawned)
             {
-                player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.transform.position, spawnPoint.transform.rotation);
-                player.transform.parent = Hus2.transform;
+                player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint2.transform.position, spawnPoint2.transform.rotation);
+                player.transform.name = "FirstPersonPlayerHus2";
                 player.transform.GetChild(1).gameObject.SetActive(true);
                 playerSpawned = true;
             }
