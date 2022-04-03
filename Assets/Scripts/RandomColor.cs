7dec66ffa44501c780508e86c9f412b9e6b54740
Daniel From Kjærgaard <KongzCringe@gmail.com>
2022-04-03 21:12:56 +0200

    maybe

--- Assets/Scripts/RandomColor.cs
+++ Assets/Scripts/RandomColor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using TMPro;
 using Photon.Pun;
@@ -16,18 +17,21 @@ public class RandomColor : MonoBehaviourPun
     [SerializeField] private TextMeshPro[] ColorBricks;
     [SerializeField] private Material[] StickyNotes; 
     [SerializeField] public string Code;
-    private List<Color> ColorsList = new List<Color>();
+    private Color[] ColorsList = new Color[4];
 
     private int numberCode;
     private int RandomTal;
     private int CodeInt;
-    private Color Color;
+    private Color _color;
 
     [PunRPC]
-    void ShareCode(string a, List<Color> noteColor)
+    void ShareCode(string a, Vector3 color1, Vector3 color2, Vector3 color3, Vector3 color4)
     {
         Code = a;
-        ColorsList = noteColor;
+        ColorsList[0] = new Color(color1.x, color1.y, color1.z);
+        ColorsList[1] = new Color(color2.x, color2.y, color2.z);
+        ColorsList[2] = new Color(color3.x, color3.y, color3.z);
+        ColorsList[3] = new Color(color4.x, color4.y, color4.z);
     }
 
     void Start()
@@ -40,18 +44,21 @@ public class RandomColor : MonoBehaviourPun
                 RandomTal = Random.Range(0, Colors.Count);  
 
                 Code += Codes[numberCode];
-                Color = Colors[RandomTal];
+                _color = Colors[RandomTal];
 
-                ColorBricks[i].GetComponent<TMP_Text>().color = Color;
-                StickyNotes[Codes[numberCode]-1].color = Color;
-                
-                ColorsList.Add(Color);
+                ColorBricks[i].GetComponent<TMP_Text>().color = _color;
+                StickyNotes[Codes[numberCode]-1].color = _color;
+
+                ColorsList[i] = _color;
             
                 Colors.RemoveAt(RandomTal);
                 Codes.RemoveAt(numberCode);
             }
             
-            photonView.RPC("ShareCode", RpcTarget.OthersBuffered, Code, ColorsList);
+            photonView.RPC("ShareCode", RpcTarget.OthersBuffered, Code, new Vector3(ColorsList[0].r, ColorsList[0].g, ColorsList[0].b),
+                new Vector3(ColorsList[1].r, ColorsList[1].g, ColorsList[1].b),
+                new Vector3(ColorsList[2].r, ColorsList[2].g, ColorsList[2].b),
+                new Vector3(ColorsList[3].r, ColorsList[3].g, ColorsList[3].b));
         }
         else
         {
