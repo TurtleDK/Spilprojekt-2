143f5a7aa00a80c1b557755d5a8f16233a4a5542
Daniel From Kjærgaard <KongzCringe@gmail.com>
2022-04-07 09:36:42 +0200

    yoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo

--- Assets/Scripts/Dragging.cs
+++ Assets/Scripts/Dragging.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
-
+using UnityEngine.PlayerLoop;
 
 
 public class Dragging : MonoBehaviour
@@ -12,19 +12,32 @@ public class Dragging : MonoBehaviour
 {
     private Vector3 mOffset;
     private float mZCoord;
+    private Rigidbody rb;
+    private bool isDragging = false;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //rb.interpolation = RigidbodyInterpolation.Interpolate;
+    }
+
     void OnMouseDown()
     {
         mZCoord = Camera.main.WorldToScreenPoint(
             gameObject.transform.position).z;
         // Store offset = gameobject world pos - mouse world pos
-        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
-        gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        //mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        isDragging = true;
+        rb.useGravity = false;
     }
 
     private void OnMouseUp()
     {
-        gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        rb.constraints = RigidbodyConstraints.None;
+        isDragging = false;
+        rb.useGravity = true;
+        //gameObject.GetComponent<Rigidbody>().isKinematic = false;
     }
 
     private Vector3 GetMouseAsWorldPoint()
@@ -37,10 +50,23 @@ public class Dragging : MonoBehaviour
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
+    private void FixedUpdate()
+    {
+        if (isDragging && rb.position != GetMouseAsWorldPoint() + mOffset)
+        {
+            rb.velocity =((GetMouseAsWorldPoint() + mOffset)-rb.position).normalized*10;
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
     void OnMouseDrag()
     {
-        gameObject.GetComponent<Rigidbody>().MovePosition(GetMouseAsWorldPoint() + mOffset);
-        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        //gameObject.GetComponent<Rigidbody>().mo
+        //gameObject.GetComponent<Rigidbody>().MovePosition(GetMouseAsWorldPoint() + mOffset);
+        //gameObject.GetComponent<Rigidbody>().isKinematic = true;
     }
 
 }
